676bf4f2d23407cf13a3a8798c13da278e77ce31
Jefferson <jeffersonbraster@gmail.com>
2021-02-06 12:54:43 -0300

    montando o grpc server

--- go.mod
+++ go.mod
@@ -3,14 +3,19 @@ module github.com/jeffersonbraster/codepix
 go 1.15
 
 require (
-  github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef
-	github.com/confluentinc/confluent-kafka-go v1.5.2
-	github.com/go-playground/validator/v10 v10.4.1
+	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef
+	github.com/golang/protobuf v1.4.2
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.9.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/satori/go.uuid v1.2.0
+	github.com/spf13/cobra v1.1.1
+	github.com/spf13/viper v1.7.0
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/grpc v1.35.0
+	google.golang.org/protobuf v1.25.0
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.20.12
--- infra/repository/pix.go
+++ infra/repository/pix.go
@@ -4,73 +4,63 @@ import (
 	"fmt"
 
 	"github.com/jeffersonbraster/codepix/domain/model"
-	"gorm.io/gorm"
+	"github.com/jinzhu/gorm"
 )
 
 type PixKeyRepositoryDb struct {
 	Db *gorm.DB
 }
 
-func(r PixKeyRepositoryDb) AddBank(bank *model.Bank) error {
+func (r PixKeyRepositoryDb) AddBank(bank *model.Bank) error {
 	err := r.Db.Create(bank).Error
-
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func(r PixKeyRepositoryDb) AddAccount(account *model.Account) error {
+func (r PixKeyRepositoryDb) AddAccount(account *model.Account) error {
 	err := r.Db.Create(account).Error
-
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func(r PixKeyRepositoryDb) RegisterKey(pixKey *model.PixKey) error {
+func (r PixKeyRepositoryDb) RegisterKey(pixKey *model.PixKey) (*model.PixKey, error) {
 	err := r.Db.Create(pixKey).Error
-
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+	return pixKey, nil
 }
 
-func(r PixKeyRepositoryDb) FindKeyById(key string, kind string) (*model.PixKey, error) {
+func (r PixKeyRepositoryDb) FindKeyByKind(key string, kind string) (*model.PixKey, error) {
 	var pixKey model.PixKey
-
 	r.Db.Preload("Account.Bank").First(&pixKey, "kind = ? and key = ?", kind, key)
 
 	if pixKey.ID == "" {
 		return nil, fmt.Errorf("no key was found")
 	}
-
 	return &pixKey, nil
 }
 
-func(r PixKeyRepositoryDb) FindAccount(id string) (*model.Account, error) {
+func (r PixKeyRepositoryDb) FindAccount(id string) (*model.Account, error) {
 	var account model.Account
-
 	r.Db.Preload("Bank").First(&account, "id = ?", id)
 
 	if account.ID == "" {
 		return nil, fmt.Errorf("no account found")
 	}
-
 	return &account, nil
 }
 
-func(r PixKeyRepositoryDb) FindBank(id string) (*model.Bank, error) {
+func (r PixKeyRepositoryDb) FindBank(id string) (*model.Bank, error) {
 	var bank model.Bank
-
-	r.Db.Preload("Bank").First(&bank, "id = ?", id)
+	r.Db.First(&bank, "id = ?", id)
 
 	if bank.ID == "" {
-		return nil, fmt.Errorf("no account found")
+		return nil, fmt.Errorf("no bank found")
 	}
-
 	return &bank, nil
 }
-
